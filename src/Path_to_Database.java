15bdafdae0be28f043540c589d2cfdb7a4af66c5
Fernanda <fe.fs@hotmail.com>
2023-09-01 13:32:11 -0400

    create a folder to store the .txt files of the database

--- src/Path_to_Database.java
+++ src/Path_to_Database.java
@@ -1,5 +1,5 @@
 public class Path_to_Database {
-    public  static String database = "C:\\Users\\ferna\\Desktop\\BAS\\SOFT_DEV1\\LMS\\LMS\\src\\Books_Database.txt";
+    public  static String database = "C:\\Users\\ferna\\Desktop\\BAS\\SOFT_DEV1\\LMS\\LMS\\src\\Database\\Books_Database.txt";
 
-    public  static String databaseBackup = "C:\\Users\\ferna\\Desktop\\BAS\\SOFT_DEV1\\LMS\\LMS\\src\\Backup_Books_Database.txt";
+    public  static String databaseBackup = "C:\\Users\\ferna\\Desktop\\BAS\\SOFT_DEV1\\LMS\\LMS\\src\\Database\\Backup_Books_Database.txt";
 }
