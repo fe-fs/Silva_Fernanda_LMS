92d64470c72e369e4bd81b379e0105c802ac336a
Fernanda <fe.fs@hotmail.com>
2023-10-14 21:02:43 -0400

    Add JavaDoc comments and fix for some paths to use variable name

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,19 +11,34 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class name: LibraryTest
+ * This class is used for testing the Library class. It creates a temporary file and loads some books into the library before each test.
+ * It also overrides some methods from the Library class to prevent interference with the tests.
+ */
 class LibraryTest {
     File tempFile;
     Library library;
-    String BooksTest_Database;
+    static String BooksTest_Database;
 
-    //Override Backup from original methods to not interfere with test
+    /**
+     * This is a subclass of Library that overrides some methods for testing purposes.
+     */
     class TestableLibrary extends Library {
+
+        /**
+         * Overrides the backupDatabase method from the Library class.
+         * This method does nothing in the test class.
+         */
         @Override
         public void backupDatabase(String Books_Database, String databaseBackup) {
             // Do nothing
         }
 
-
+        /**
+         * Overrides the updateDatabase method from the Library class.
+         * This method calls the superclass's updateDatabase method with the path of the temporary file.
+         */
         @Override
         public void updateDatabase(String filename) throws IOException {
             super.updateDatabase(tempFile.getPath());
@@ -30,7 +46,11 @@ class LibraryTest {
 
     }
 
-        //setUp method creates a new temporary file and loads some books into the library before each test.
+        /**
+         * Method name: setUp
+         * This method is run before each test. It creates a new temporary file and loads some books into the library.
+         * @throws IOException if an I/O error occurs when creating the file or writing to it
+         */
         @BeforeEach
         void setUp() throws IOException {
             // Create a temporary file
@@ -50,13 +70,20 @@ class LibraryTest {
             library.loadBooksFromFile(tempFile.getPath());
         }
 
+        /**
+         * Method name: tearDown
+         * This method is run after all tests in the class have finished. It deletes the temporary file used in the tests.
+         * @throws IOException if an I/O error occurs when deleting the file
+         */
+        @AfterAll
+        static void tearDown() throws IOException {
+                Files.delete(Path.of(BooksTest_Database));
+        }
 
-   /* @AfterEach
-    void tearDown() throws IOException {
-        Files.delete(tempFile.toPath());
-    }*/
-
-
+        /**
+         * Method name: testLoadBooksFromFile
+         * This method tests the loadBooksFromFile method of the Library class. It verifies that the books were loaded correctly from the file and prints the list of books.
+         */
         @Test
         void testLoadBooksFromFile() {
             System.out.println("Test loadBooksFromFile method");
@@ -67,7 +94,12 @@ class LibraryTest {
             library.listBooks(BooksTest_Database);
         }
 
-
+        /**
+         * Method name: testAddBookToFile
+         * This method tests the addBookToFile method of the Library class. It simulates user input to add a new book to the library,
+         * then verifies that the book was added correctly by comparing the expected and actual book details.
+         * @throws IOException if an I/O error occurs when writing to or reading from the file
+         */
         @Test
         void testAddBookToFile() throws IOException {
             System.out.println("loadBooksFromFile method Test");
@@ -108,13 +140,10 @@ class LibraryTest {
 
 
         /**
-         * testRemoveBookID
-         * this test is essentially comparing the state of the library before and after the removeBook method is called.
-         * Before the method is called, there is a book with ID removedBookId in the library
-         * After the method is called, there should be no books with that ID in the library.
-         * If there are still books with that ID in the library, then the test will fail.
-         *
-         * @throws IOException
+         * Method name: testRemoveBookID
+         * This method tests the removeBookID method of the Library class. It removes a book from the library by its ID,
+         * then verifies that the book was removed correctly by checking that no book in the library has the removed book's ID.
+         * @throws IOException if an I/O error occurs when reading from or writing to the file
          */
         @Test
         void testRemoveBookID() throws IOException {
@@ -128,10 +157,10 @@ class LibraryTest {
             int removedBookId = bookToRemove.getId();
 
             // Act
-            library.removeBookID(removedBookId, tempFile.getPath());
+            library.removeBookID(removedBookId, BooksTest_Database);
 
             // Load the books from the file again
-            library.loadBooksFromFile(tempFile.getPath());
+            library.loadBooksFromFile(BooksTest_Database);
 
             // Assert
             boolean isRemoved = library.getBooks().stream().noneMatch(book -> book.getId() == removedBookId);
@@ -144,6 +173,12 @@ class LibraryTest {
             library.listBooks(BooksTest_Database);
         }
 
+        /**
+         * Method name: testRemoveBookBarcode
+         * This method tests the removeBookBarcode method of the Library class. It removes a book from the library by its barcode,
+         * then verifies that the book was removed correctly by checking that no book in the library has the removed book's barcode.
+         * @throws IOException if an I/O error occurs when reading from or writing to the file
+         */
         @Test
         void testRemoveBookBarcode() throws IOException {
             // Print list
@@ -158,7 +193,7 @@ class LibraryTest {
             library.removeBookBarcode(removedBookBarcode, true);
 
             // Load the books from the file again
-            library.loadBooksFromFile(tempFile.getPath());
+            library.loadBooksFromFile(BooksTest_Database);
 
             // Assert
             boolean isRemoved = library.getBooks().stream().noneMatch(book -> book.getBarcode().equals(removedBookBarcode));
@@ -171,104 +206,114 @@ class LibraryTest {
             library.listBooks(BooksTest_Database);
         }
 
+        /**
+         * Method name: testRemoveBookByTitle
+         * This method tests the removeBookByTitle method of the Library class. It removes a book from the library by its title,
+         * then verifies that the book was removed correctly by checking that no book in the library has the removed book's title.
+         * @throws IOException if an I/O error occurs when reading from or writing to the file
+         */
+        @Test
+        void testRemoveBookByTitle() throws IOException {
+            // Print list
+            System.out.println("Before remove List:");
+            library.listBooks(BooksTest_Database);
+            System.out.print("\n");
 
-    @Test
-    void testRemoveBookByTitle() throws IOException {
-        // Print list
-        List<Book> books = library.getBooks();
-        System.out.println("Before remove List:");
-        for (Book book : books) {
-            System.out.println(book);
+            // Get the book to be removed
+            String titleToRemove = library.getBooks().get(0).getTitle();
+
+            // Act
+            library.removeBookByTitle(titleToRemove);
+
+            // Check if there are no books in the library that have the title titleToRemove.
+            // If the result is true, then the book has been successfully removed. If it’s false, then the book is still in the library.
+            // this lambda expression (->) takes a book as input and returns a boolean indicating whether the title of the book equals titleToRemove
+            boolean isRemoved = library.getBooks().stream().noneMatch(book -> book.getTitle().equals(titleToRemove));
+
+            //Assert
+            assertTrue(isRemoved, "The book was not removed successfully");
+
+            //print list
+            System.out.print("\n");
+            System.out.println("After remove List:");
+            library.listBooks(BooksTest_Database);
         }
-        System.out.print("\n");
 
-        // Get the book to be removed
-        String titleToRemove = library.getBooks().get(0).getTitle();
 
-        // Act
-        library.removeBookByTitle(titleToRemove);
+        /**
+         * Method name: testCheckoutBook
+         * This method tests the checkoutBook method of the Library class. It checks out a book from the library by its title,
+         * then verifies that the book was checked out correctly by checking that the book's check status is 'checkedOut'.
+         * @throws IOException if an I/O error occurs when reading from or writing to the file
+         */
+        @Test
+        void testCheckoutBook() throws IOException {
+            // Print list
+            System.out.println("Before remove List:");
+            library.listBooks(BooksTest_Database);
+            System.out.print("\n");
 
-        // Assert
-        boolean isRemoved = library.getBooks().stream().noneMatch(book -> book.getTitle().equals(titleToRemove));
-        assertTrue(isRemoved, "The book was not removed successfully");
+            // Get the book to be checked out
+            String titleToCheckout = library.getBooks().get(1).getTitle();
 
-        System.out.print("\n");
-        // Print list
-        System.out.println("After remove List:");
-        for (Book book : books) {
-            System.out.println(book);
+            // Act
+            boolean isCheckoutSuccessful = library.checkoutBook(titleToCheckout, BooksTest_Database);
+
+            // Assert
+            assertTrue(isCheckoutSuccessful, "The book was not checked out successfully");
+
+            // Load the books from the file again
+            library.loadBooksFromFile(BooksTest_Database);
+
+            // Assert that the book is checked out
+            //Find the first book in the library that has the title titleToCheckout.
+            // If such a book exists, assign it to checkedOutBook. If no such book exists, assign null to checkedOutBook
+            Book checkedOutBook = library.getBooks().stream().filter(book -> book.getTitle().equals(titleToCheckout)).findFirst().orElse(null);
+            assertNotNull(checkedOutBook, "The book was not found after checkout");
+            assertEquals("checkedOut", checkedOutBook.getCheckStatus(), "The book's check status is not 'checkedOut'");
+
+            //print list
+            System.out.print("\n");
+            System.out.println("After remove List:");
+            library.listBooks(BooksTest_Database);
         }
-    }
 
-    @Test
-    void testCheckoutBook() throws IOException {
-        // Print list
-        List<Book> books = library.getBooks();
-        System.out.println("Before checkout List:");
-        for (Book book : books) {
-            System.out.println(book);
+        /**
+         * Method name: testCheckInBook
+         * This method tests the checkInBook method of the Library class. It checks in a book to the library by its title,
+         * then verifies that the book was checked in correctly by checking that the book's check status is 'checkedIn'.
+         * @throws IOException if an I/O error occurs when reading from or writing to the file
+         */
+        @Test
+        void testCheckInBook() throws IOException {
+            // Print list
+            System.out.println("Before remove List:");
+            library.listBooks(BooksTest_Database);
+            System.out.print("\n");
+
+            // Get the book to be checked in
+            String titleToCheckIn = library.getBooks().get(0).getTitle();
+
+            // Act
+            boolean isCheckInSuccessful = library.checkInBook(titleToCheckIn, BooksTest_Database);
+
+            // Assert
+            assertTrue(isCheckInSuccessful, "The book was not checked in successfully");
+
+            // Load the books from the file again
+            library.loadBooksFromFile(BooksTest_Database);
+
+            // Assert that the book is checked in
+            //Find the first book in the library that has the title titleToCheckIn.
+            // If such a book exists, assign it to checkedInBook. If no such book exists, assign null to checkedInBook
+            Book checkedInBook = library.getBooks().stream().filter(book -> book.getTitle().equals(titleToCheckIn)).findFirst().orElse(null);
+            assertNotNull(checkedInBook, "The book was not found after check-in");
+            assertEquals("checkedIn", checkedInBook.getCheckStatus(), "The book's check status is not 'checkedIn'");
+
+            //print list
+            System.out.print("\n");
+            System.out.println("After remove List:");
+            library.listBooks(BooksTest_Database);
         }
-        System.out.print("\n");
-
-        // Get the book to be checked out
-        String titleToCheckout = library.getBooks().get(1).getTitle();
-
-        // Act
-        boolean isCheckoutSuccessful = library.checkoutBook(titleToCheckout, tempFile.getPath());
-
-        // Assert
-        assertTrue(isCheckoutSuccessful, "The book was not checked out successfully");
-
-        // Load the books from the file again
-        library.loadBooksFromFile(tempFile.getPath());
-
-        // Assert that the book is checked out
-        Book checkedOutBook = library.getBooks().stream().filter(book -> book.getTitle().equals(titleToCheckout)).findFirst().orElse(null);
-        assertNotNull(checkedOutBook, "The book was not found after checkout");
-        assertEquals("checkedOut", checkedOutBook.getCheckStatus(), "The book's check status is not 'checkedOut'");
-
-        System.out.print("\n");
-        // Print list
-        System.out.println("After checkout List:");
-        for (Book book : books) {
-            System.out.println(book);
-        }
-    }
-
-    @Test
-    void testCheckInBook() throws IOException {
-        // Print list
-        List<Book> books = library.getBooks();
-        System.out.println("Before check-in List:");
-        for (Book book : books) {
-            System.out.println(book);
-        }
-        System.out.print("\n");
-
-        // Get the book to be checked in
-        String titleToCheckIn = library.getBooks().get(0).getTitle();
-
-        // Act
-        boolean isCheckInSuccessful = library.checkInBook(titleToCheckIn, tempFile.getPath());
-
-        // Assert
-        assertTrue(isCheckInSuccessful, "The book was not checked in successfully");
-
-        // Load the books from the file again
-        library.loadBooksFromFile(tempFile.getPath());
-
-        // Assert that the book is checked in
-        Book checkedInBook = library.getBooks().stream().filter(book -> book.getTitle().equals(titleToCheckIn)).findFirst().orElse(null);
-        assertNotNull(checkedInBook, "The book was not found after check-in");
-        assertEquals("checkedIn", checkedInBook.getCheckStatus(), "The book's check status is not 'checkedIn'");
-
-        System.out.print("\n");
-        // Print list
-        System.out.println("After check-in List:");
-        for (Book book : books) {
-            System.out.println(book);
-        }
-    }
-
 
     }
